16b99c45154e568de365761645be3524741f533e
hanmose <nightsound5124@gmail.com>
2026-05-12 14:46:01 +0900

    0512

---
+++ src/main/java/kr/ac/kopo/mose/_026example/controller/Ex10Controller.java
@@ -0,0 +1,7 @@
+package kr.ac.kopo.mose._026example.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Ex10Controller {
+}
--- src/main/java/kr/ac/kopo/mose/_026example/domain/Member.java
+++ src/main/java/kr/ac/kopo/mose/_026example/domain/Member.java
@@ -1,13 +1,17 @@
 package kr.ac.kopo.mose._026example.domain;
 
 import lombok.Data;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
 
-@Data // 이 어노테이션이 Getter, Setter, toString 등을 자동으로 만들어줍니다!
+@Data
 public class Member {
-    private String id;
-    private String passwd; // 이미지에는 int인데 비밀번호는 보통 String으로 합니다.
-    private String city;
-    private String sex;
-    private String[] hobby;
-    private String greetings;
+//    private String id;
+//    private String passwd;
+//    private String city;
+//    private String gender;
+//    private String[] hobby;
+//    private String greetings;
+    private String name;
+    private MultipartFile fileImage;
 }
