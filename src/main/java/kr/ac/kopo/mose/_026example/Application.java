9f5fbbbecce424fd9113741de062d9faba65bd7d
hanmose <nightsound5124@gmail.com>
2026-06-09 11:46:21 +0900

    사용자 정보 검색 유형 중에 Authentication 유형을 사용함

--- src/main/java/kr/ac/kopo/mose/_026example/Application.java
+++ src/main/java/kr/ac/kopo/mose/_026example/Application.java
@@ -1,5 +1,6 @@
 package kr.ac.kopo.mose._026example;
 
+import kr.ac.kopo.mose._026example.configure.SecurityConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-        // 이 문장이 Spring Boot를 실행시키는 핵심 Main 메소드입니다.
         SpringApplication.run(Application.class, args);
     }
 
