a0fe742fcd2796653641ce41808bff22cff75588
hanmose <nightsound5124@gmail.com>
2026-06-16 11:43:38 +0900

    사용자정의 로그인 화면 설정과 로그아웃

--- src/main/java/kr/ac/kopo/mose/_026example/controller/Chap10_01Controller.java
+++ src/main/java/kr/ac/kopo/mose/_026example/controller/Chap10_01Controller.java
@@ -1,4 +1,4 @@
-package kr.ac.kopo.minn._026example.controller;
+package kr.ac.kopo.mose._026example.controller;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
@@ -81,5 +81,24 @@ public class Chap10_01Controller {
     @GetMapping("/admin/tag")
     public String requestMethod11(Model model){
         return "viewPage10_04";
+    }
+
+//    사용자 정의 로그인과 관련
+
+    @GetMapping("/exam05")
+    public String requestMethod12(Model model){
+        return "viewPage10_05";
+    }
+
+    @GetMapping("/admin")
+    public String requestMethod13(Authentication user,  Model model){
+        model.addAttribute("username", user.getName());
+        return "viewPage10_05_result";
+    }
+
+    //    로그아웃
+    @GetMapping("/logout")
+    public String requestMethod14(Model model){
+        return "viewPage10_05";
     }
 }
